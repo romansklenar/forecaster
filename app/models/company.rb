f1d8c51a57f9819fd6bd993fc01cb402b1206137
Roman Sklenář <mail@romansklenar.cz>
2013-01-14 12:56:20 +0100

    implemented validators & after_create callbacks to Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,17 @@
 class Company < ActiveRecord::Base
+  has_many :messages,        dependent: :destroy
+  has_many :stocks,          dependent: :destroy
+  has_many :classifications, dependent: :destroy
+
+  validates :name, :code, :messages_url, :stocks_url, presence: true
+  validates :code, uniqueness: true
+
+  after_create :import_stocks
+  after_create :import_messages
+
+  def import_stocks
+  end
+
+  def import_messages
+  end
 end
